a05f957af301f3554b53ce66338975907f8aa7bb
romeShowmethecode <romeoluca7531@gmail.com>
2023-11-09 17:34:22 +0100

    Added routes for main page and to apply for a proposal

---
+++ frontend/src/pages/ApplyToProposal.jsx
@@ -0,0 +1,7 @@
+function ApplyToProposal(props){
+    return(
+        <h1>That's the page to apply for a proposal</h1>
+    )
+}
+
+export default ApplyToProposal;
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,7 @@
+function MainPage(props){
+    return(
+        <h1>This will be the main page</h1>
+    )
+}
+
+export default MainPage;

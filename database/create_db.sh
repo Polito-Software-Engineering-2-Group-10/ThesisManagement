686ddec23e8e3fbe1057539320ed9e97cac809df
Alessio <53220763+Atari2@users.noreply.github.com>
2023-11-09 12:10:03 +0100

    Add start of docker implementation (no documentation yet)

---
+++ database/create_db.sh
@@ -0,0 +1,3 @@
+# this script is used by the Dockerfile to initialize the database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /create_tables.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /init_data.sql

97d6824c370efa5b727549396b9be978957ab62b
romeShowmethecode <romeoluca7531@gmail.com>
2023-11-11 18:51:44 +0100

    Added the prototype of professor main page

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -9,22 +9,39 @@ function MainPage(props){
                 <Row>
                     <Col style={{textAlign: "center"}}>
                         <h1 style={{textDecorationLine: "underline"}}>Thesis Managment Menu</h1>
-                        <p>Here you can manage all your thesis applications and see if there are some interesting proposals for you.</p>
-                        <br></br>
-                        <ul style={{textAlign: "center", listStyle: 'none', fontWeight: 'bold'}}>
-                            <li>Name and Surname of the student</li>
-                            <li>Type of degree</li>
-                            <li>Enrollment Year</li>
-                        </ul>
+                        {/* student info */}
+                        <div style={{display: "none"}}>
+                            <p>Here you can manage all your thesis applications and see if there are some interesting proposals for you.</p>
+                            <br></br>
+                            <ul style={{textAlign: "center", listStyle: 'none', fontWeight: 'bold'}}>
+                                <li>Name and Surname of the student</li>
+                                <li>Type of degree</li>
+                                <li>Enrollment Year</li>
+                            </ul>
+                        </div>
+                        
+                        {/* professor info */}
+                        <div>
+                            <p>Here you can manage all your thesis proposals and see the state of them all.</p>
+                            <br></br>
+                            <ul style={{ textAlign: "center", listStyle: 'none', fontWeight: 'bold'}}>
+                                <li>Name and Surname of the professor</li>
+                                <li>Group of the professor</li>
+                                <li>Department</li>
+                            </ul>
+                        </div>
+                        
+                        
                     </Col>
                 </Row>
-                <Row style={{paddingTop: "60px"}}>
+                {/* professor interface */}
+                <Row style={{paddingTop: "30px", display:"none"}}>
                     <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
                     <Card style={{ width: '18rem', cursor: 'pointer', margin: '0 auto'}} bg="primary" text="light" className="mb-2">
                         <Card.Body>
                             <Card.Title>Search for a thesis proposal</Card.Title>
                             <Card.Text>
-                            And choose which apply for
+                                And choose which apply for
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -33,6 +50,39 @@ function MainPage(props){
                         <Card style={{ width: '18rem', cursor: 'pointer', margin: '0 auto', verticalAlign: "center"}} bg="primary" text="light" className="mb-2">
                         <Card.Body>
                             <Card.Title>Browse Application Decisions</Card.Title>
+                            <Card.Text>
+                                To see how your applications went
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                </Row>
+                {/* student cards */}
+                <Row style={{paddingTop: "30px"}}>
+                    <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <Card style={{ width: '18rem', cursor: 'pointer', margin: '0 auto'}} bg="primary" text="light" className="mb-2">
+                        <Card.Body>
+                            <Card.Title>Insert a new thesis proposal</Card.Title>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                        <Card style={{ width: '18rem', cursor: 'pointer', margin: '0 auto', verticalAlign: "center"}} bg="primary" text="light" className="mb-2">
+                        <Card.Body>
+                            <Card.Title>Browse all Applications</Card.Title>
+                            <Card.Text>
+                                if you want to accept or reject one
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                        <Card style={{ width: '18rem', cursor: 'pointer', margin: '0 auto', verticalAlign: "center"}} bg="primary" text="light" className="mb-2">
+                        <Card.Body>
+                            <Card.Title>Browse proposals</Card.Title>
+                            <Card.Text>
+                                and operate on them
+                            </Card.Text>
                         </Card.Body>
                     </Card>
                     </Col>

7dc8580fed204bb7033302266ddb3a5d759af467
Xiong233333 <457370806@qq.com>
2023-11-13 13:33:21 +0100

    Create BrowseProposal

---
+++ frontend/src/pages/BrowseProposal.jsx
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from "react";
+import { getProposals } from "../API";
+import { Container, Col, Row,Table } from "react-bootstrap";
+
+function BrowseProposal (props){
+  const [data, setData] = useState([]);
+  const fetchData = async () =>{
+    const result = await getProposals();
+    setData(result.proposalSummary);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+    
+  return (
+    <Container>
+    <Table striped>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Expiration</th>
+        <th>Level</th>
+        <th>Type</th>
+      </tr>
+    </thead>
+    <tbody>
+    {data.map((proposalSummary, index) => (
+        <tr>
+          <td>{proposalSummary.thesis_title}</td>
+          <td>{proposalSummary.thesis_expiration}</td>
+          <td>{proposalSummary.thesis_level}</td>
+          <td>{proposalSummary.thesis_type}</td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+  </Container>
+  )
+}
+
+
+export default BrowseProposal;

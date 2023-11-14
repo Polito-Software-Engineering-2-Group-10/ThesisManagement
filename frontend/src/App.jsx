768b2955d8daa179c0ea37d54efcd34ec1ff6cea
s313372 <126325821+s313372@users.noreply.github.com>
2023-11-14 11:26:02 +0100

    Adding SearchForProposals page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ApplyToProposal from './pages/ApplyToProposal';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
+import SearchForProposals from "./pages/SearchForProposals.jsx";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/*' element={<MainPage/>}></Route>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/apply' element={<ApplyToProposal/>}></Route>
+        <Route path='/search' element={<SearchForProposals/>}></Route>
       </Routes>
     </BrowserRouter>
     </>

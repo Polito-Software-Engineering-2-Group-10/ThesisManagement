44d6b6020592598b5edd66be0341a8f838e5891b
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2023-11-27 11:42:04 +0100

    refactor browse proposal
    Added component browse proposal and created colors css

--- frontend/src/pages/BrowseProposal.jsx
+++ frontend/src/pages/BrowseProposal.jsx
@@ -1,26 +1,62 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button,Container, Table, Row, Col } from "react-bootstrap";
 import { Navigation } from "./Navigation";
 import { useNavigate} from "react-router-dom";
 import dayjs from 'dayjs'
+import "../styles/BrowseProposal.css";
 
 function BrowseProposal (props){
-  const navigate = useNavigate();
-  
-  const [selectedProposal, setSelectedProposal] = useState(null);
-  
-  const handleProposalClick = (proposal) => {
-    setSelectedProposal(proposal);
-  }
+   
+    // utility to generate a table row from a proposal
+    const [activeProposals, setActiveProposals] = useState(null);
+    const [archivedProposals, setArchivedProposals] = useState(null);
     
-  const handleCopyClick = (proposal) => {
-    // navigate to insert proposal page with the proposal as a parameter
-    navigate('/insert', {state: {proposal: proposal}});
-  }
+    useEffect(() => {
+        if (props.proposalList != null) {
+            setActiveProposals(props.proposalList.active);
+            setArchivedProposals(props.proposalList.archived);
+        }
+        console.log(activeProposals);
+    }, [props.proposalList]);
+
+    return (
+    <>
+        <Navigation logout={props.logout} loggedIn={props.loggedIn} user={props.user}/>
+
+        <Container>
+            <ProposalTable title="Active proposals" proposalList={activeProposals} />
+            <ProposalTable title="Archived proposals" proposalList={archivedProposals} />
+        </Container>
+    </>
+  )
+}
+
+
+function ProposalTable(props){
+    const navigate = useNavigate();
     
-  // utility to generate a table row from a proposal
-  const generateTable = (result) => {
+    // title of the table
+    let {title} = props;
+
+    // list of proposal 
+    const [proposalList, setProposalList] = useState(null);
+    const [selectedProposal, setSelectedProposal] = useState(null);
+    
+    // update proposal list
+    useEffect(() => {
+        if (props.proposalList != null) {
+            setProposalList(props.proposalList);
+        }
+    }, [props.proposalList]);
+  
+    // click on a proposal
+    const handleProposalClick = (proposal) => {
+        setSelectedProposal(proposal);
+    }   
+
+    // generates the row for a proposal
+    const generateRow = (result) => {
         return (
          <tr key={result.id} onClick={() => handleProposalClick(result)}
         className={selectedProposal && selectedProposal.id === result.id ? 'table-primary' : ''}
@@ -29,73 +65,69 @@ function BrowseProposal (props){
           <td>{dayjs(result.expiration).format('DD/MM/YYYY')}</td>
           <td>{result.level==1 ? "Bachelor" : "Master"}</td>
           <td>{result.type}</td>
-          <td><Button onClick={()=>handleCopyClick(result)}><i className="bi bi-copy"></i></Button></td>
+          <td>
+            <span style={{display: "flex"}}>
+                
+                <Button className="btn-edit" onClick={()=>{}}><i className="bi bi-pencil-square"></i></Button>
+                <Button onClick={()=>handleCopyClick(result)}><i className="bi bi-copy"></i></Button>
+            </span>
+          </td>
         </tr>
+        )
+    }
+
+    const handleCopyClick = (proposal) => {
+        // navigate to insert proposal page with the proposal as a parameter
+        navigate('/insert', {state: {proposal: proposal}});
+    }
+  
+    return (
+        <Container className="proposal-table">
+        <Row>
+            <h3 className='center-text'>{title}</h3>
+        </Row>
+
+        <Table hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Expiration</th>
+            <th>Level</th>
+            <th>Type</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        {/* For story 7, professor should see only *active* proposals */}
+        {proposalList && proposalList.map((result, index) => 
+            generateRow(result)
+        )}
+        </tbody>
+      </Table>
+        {/* For story 7, professor should see only *active* proposals 
+            { props.proposalList && props.proposalList.archived.map((result, index) => (<></>))}
+            </tbody>
+          </Table>
+         */}
+            {/*
+            <Row>
+                <Col style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end'
+                }}>
+                    <Button className='my-2'  variant='success' disabled={!selectedProposal}>Modify</Button>
+                </Col>
+                <Col style={{
+                    display: 'flex',
+                    justifyContent: 'flex-start'
+                }}>
+                    <Button className='my-2'  variant='warning' onClick={()=>navigate('/')}>Back</Button>
+            </Col>
+        </Row>
+        */}
+        </Container>
     )
-  }
-
-
-
-  return (
-    <>
-    <Navigation logout={props.logout} loggedIn={props.loggedIn} user={props.user}/>
-    <Container>
-      {/*<h3>Teacher ID: </h3>*/}
-    <Row>
-        <h3 className='center-text'>Active proposals</h3>
-    </Row>
-    <Table hover>
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Expiration</th>
-        <th>Level</th>
-        <th>Type</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-    {/* For story 7, professor should see only *active* proposals */}
-    {props.proposalList && props.proposalList.active.map((result, index) => 
-        generateTable(result)
-    )}
-    </tbody>
-  </Table>
-  <Row>
-        <h3 className='center-text'>Archived proposals</h3>
-    </Row>
-    <Table hover>
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Expiration</th>
-        <th>Level</th>
-        <th>Type</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-    {/* For story 7, professor should see only *active* proposals */}
-    {props.proposalList && props.proposalList.archived.map((result, index) => generateTable(result))}
-    </tbody>
-  </Table>
-  <Row>
-        <Col style={{
-            display: 'flex',
-            justifyContent: 'flex-end'
-        }}>
-            <Button className='my-2'  variant='success' disabled={!selectedProposal}>Modify</Button>
-        </Col>
-        <Col style={{
-            display: 'flex',
-            justifyContent: 'flex-start'
-        }}>
-            <Button className='my-2'  variant='warning' onClick={()=>navigate('/')}>Back</Button>
-        </Col>
-  </Row>
-  </Container>
-  </>
-  )
 }
 
 

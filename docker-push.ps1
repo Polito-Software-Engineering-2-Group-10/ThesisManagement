c93b406b499e4b207a653c57c1b6179e4b4fefba
Alessio <53220763+Atari2@users.noreply.github.com>
2023-11-29 20:46:50 +0100

    Create script to update docker images

---
+++ docker-push.ps1
@@ -0,0 +1,15 @@
+function PushDockerImage {
+    param(
+        [Parameter(Mandatory=$true)][string]$ImageName
+    )
+    Set-Location $ImageName
+    docker build -t $ImageName .
+    docker tag $ImageName atari2/thesismanagement:$ImageName
+    docker push atari2/thesismanagement:$ImageName
+    Set-Location ..
+}
+
+PushDockerImage "backend"
+PushDockerImage "frontend"
+PushDockerImage "database"
+PushDockerImage "authsources"

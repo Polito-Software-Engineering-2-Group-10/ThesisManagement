811c87cd680e8e1e15b01dc1d6a16da610214df0
Alessio <53220763+Atari2@users.noreply.github.com>
2023-11-29 19:10:32 +0100

    Format and update API.jsx

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -10,15 +10,15 @@ async function setVirtualClock(date) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({date: date})
+        body: JSON.stringify({ date: date })
     });
     if (response.ok) {
         const respDetail = await response.json();
         return respDetail;
-      } else {
+    } else {
         const errDetail = await response.json();
         throw errDetail;
-      }
+    }
 }
 
 async function resetVirtualClock() {
@@ -29,29 +29,29 @@ async function resetVirtualClock() {
     if (response.ok) {
         const respDetail = await response.json();
         return respDetail;
-      } else {
+    } else {
         const errDetail = await response.json();
         throw errDetail;
-      }
+    }
 }
 
 async function addProposal(proposal) {
-  let response = await fetch(URL + '/teacher/insertProposal', {
-    credentials: 'include',
-    method: 'POST',
-    
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(proposal)
-  });
-  if (response.ok) {
-    const respDetail = await response.json();
-    return respDetail;
-  } else {
-    const errDetail = await response.json();
-    throw errDetail;
-  }
+    let response = await fetch(URL + '/teacher/insertProposal', {
+        credentials: 'include',
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(proposal)
+    });
+    if (response.ok) {
+        const respDetail = await response.json();
+        return respDetail;
+    } else {
+        const errDetail = await response.json();
+        throw errDetail;
+    }
 }
 // login,logout,session
 
@@ -65,140 +65,140 @@ async function logOutWithSaml() {
 
 async function logIn(credentials) {
     let response = await fetch(URL + '/login', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    });
-    if (response.ok) {
-      const user = await response.json();
-      return user;
-    } else {
-      const errDetail = await response.json();
-      throw errDetail.message;
-    }
-  }
-  
-  async function logOut() {
-    await fetch(URL+'/logout', {
-      method: 'DELETE', 
-      credentials: 'include' 
-    });
-  }
-  
-  async function getUserInfo() {
-    const response = await fetch(URL+'/session', {
-      credentials: 'include'
-    });
-    const userInfo = await response.json();
-    if (response.ok) {
-      return userInfo;
-    } else {
-      throw userInfo;  // object with error returned from server
-    }
-  }
-
-  async function getTeacherDetail() {
-    const response = await fetch(URL+'/teacher/details', {
-      credentials: 'include'
-    });
-    const teacherDetail = await response.json();
-    if (response.ok) {
-      return teacherDetail;
-    } else {
-      throw teacherDetail;
-    }
-  }
-
-  async function getStudentDetail() {
-    const response = await fetch(URL+'/student/details', {
-      credentials: 'include'
-    });
-    const studentDetail = await response.json();
-    if (response.ok) {
-      return studentDetail;
-    } else {
-      throw studentDetail;
-    }
-  }
-
-  async function getAllProposals() {
-    const response = await fetch(URL+'/ProposalsList', {
-      credentials: 'include'
-    });
-    const propList = await response.json();
-    if (response.ok) {
-      return propList;
-    } else {
-      throw propList;
-    }
-  }
-
-  async function getAllProposalsForStudent(cod_degree) {
-    const response = await fetch(URL+'/student/ProposalsList', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({cod_degree:cod_degree}),
-    });
-    const propList = await response.json();
-    if (response.ok) {
-      return propList;
-    } else {
-      throw propList;
-    }
-  }
-  
-  async function getApplicationsList() {
-    const response = await fetch(URL+'/student/ApplicationsList', {
-      credentials: 'include'
-    });
-    const appList = await response.json();
-    if (response.ok) {
-      return appList;
-    } else {
-      throw appList;
-    }
-  }
-
-  async function getApplicationsListTeacher() {
-    const response = await fetch(URL+'/teacher/ApplicationsList', {
-      credentials: 'include'
-    });
-    const appList = await response.json();
-    if (response.ok) {
-      return appList;
-    } else {
-      throw appList;
-    }
-  }
-
-
-  function addApplication(application) {
-    return new Promise((resolve, reject) => {
-      fetch(URL+`/student/applyProposal`, {
         method: 'POST',
         credentials: 'include',
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(Object.assign({}, application, {proposal_id: application.proposal_id, apply_date: dayjs(application.apply_date).format('YYYY-MM-DD')}))
-      }).then((response) => {
-        if (response.ok) {
-          response.json()
-            .then((id) => resolve(id))
-            .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
-        } else {
-          // analyze the cause of error
-          response.json()
-            .then((message) => { reject(message); }) // error message in the response body
-            .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
-        }
-      }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
+        body: JSON.stringify(credentials),
+    });
+    if (response.ok) {
+        const user = await response.json();
+        return user;
+    } else {
+        const errDetail = await response.json();
+        throw errDetail.message;
+    }
+}
+
+async function logOut() {
+    await fetch(URL + '/logout', {
+        method: 'DELETE',
+        credentials: 'include'
+    });
+}
+
+async function getUserInfo() {
+    const response = await fetch(URL + '/session', {
+        credentials: 'include'
+    });
+    const userInfo = await response.json();
+    if (response.ok) {
+        return userInfo;
+    } else {
+        throw userInfo;  // object with error returned from server
+    }
+}
+
+async function getTeacherDetail() {
+    const response = await fetch(URL + '/teacher/details', {
+        credentials: 'include'
+    });
+    const teacherDetail = await response.json();
+    if (response.ok) {
+        return teacherDetail;
+    } else {
+        throw teacherDetail;
+    }
+}
+
+async function getStudentDetail() {
+    const response = await fetch(URL + '/student/details', {
+        credentials: 'include'
+    });
+    const studentDetail = await response.json();
+    if (response.ok) {
+        return studentDetail;
+    } else {
+        throw studentDetail;
+    }
+}
+
+async function getAllProposals() {
+    const response = await fetch(URL + '/ProposalsList', {
+        credentials: 'include'
+    });
+    const propList = await response.json();
+    if (response.ok) {
+        return propList;
+    } else {
+        throw propList;
+    }
+}
+
+async function getAllProposalsForStudent(cod_degree) {
+    const response = await fetch(URL + '/student/ProposalsList', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cod_degree: cod_degree }),
+    });
+    const propList = await response.json();
+    if (response.ok) {
+        return propList;
+    } else {
+        throw propList;
+    }
+}
+
+async function getApplicationsList() {
+    const response = await fetch(URL + '/student/ApplicationsList', {
+        credentials: 'include'
+    });
+    const appList = await response.json();
+    if (response.ok) {
+        return appList;
+    } else {
+        throw appList;
+    }
+}
+
+async function getApplicationsListTeacher() {
+    const response = await fetch(URL + '/teacher/ApplicationsList', {
+        credentials: 'include'
+    });
+    const appList = await response.json();
+    if (response.ok) {
+        return appList;
+    } else {
+        throw appList;
+    }
+}
+
+
+function addApplication(application) {
+    return new Promise((resolve, reject) => {
+        fetch(URL + `/student/applyProposal`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(Object.assign({}, application, { proposal_id: application.proposal_id, apply_date: dayjs(application.apply_date).format('YYYY-MM-DD') }))
+        }).then((response) => {
+            if (response.ok) {
+                response.json()
+                    .then((id) => resolve(id))
+                    .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
+            } else {
+                // analyze the cause of error
+                response.json()
+                    .then((message) => { reject(message); }) // error message in the response body
+                    .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
+            }
+        }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
     });
 }
 
@@ -247,144 +247,141 @@ async function getFilteredProposals(filters) {
     return data;
 }
 
-  async function getTeacherProposals() {
-    const response = await fetch(`${URL}/teacher/ProposalsList`,{
-      credentials: 'include'
-  });
+async function getTeacherProposals() {
+    const response = await fetch(`${URL}/teacher/ProposalsList`, {
+        credentials: 'include'
+    });
     const data = await response.json();
     return data;
 }
 
 async function acceptDeclineApplication(mailInfo) {
-  const response = await fetch(`${URL}/teacher/applicationDetail/${mailInfo.id}`,{
-    method: 'PATCH',  
-    credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({status:mailInfo.status}),
-  });
-
-  const data = await response.json();
-  if(response.status==200)
-  {
-    await fetch(`${URL}/send_email`,{
-      method: 'POST',  
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-            recipient_mail: "s313372@studenti.polito.it",
-            subject: `Result on your application about ${mailInfo.thesis_title}` ,
-            message: `Hello ${mailInfo.student_gender=='M' ? 'Mr.':'Mrs.'} ${mailInfo.student_name} ${mailInfo.student_surname},\nyour thesis application for the ${mailInfo.thesis_title} proposal, supervised by professor ${mailInfo.teacher_surname}, has been ${mailInfo.status ? 'Accepted': 'Rejected'}.\nBest Regards, Polito Staff.`
-      }),
+    const response = await fetch(`${URL}/teacher/applicationDetail/${mailInfo.id}`, {
+        method: 'PATCH',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ status: mailInfo.status }),
     });
-  }
-  return data;
+
+    const data = await response.json();
+    if (response.status == 200) {
+        await fetch(`${URL}/send_email`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                recipient_mail: "s313372@studenti.polito.it",
+                subject: `Result on your application about ${mailInfo.thesis_title}`,
+                message: `Hello ${mailInfo.student_gender == 'M' ? 'Mr.' : 'Mrs.'} ${mailInfo.student_name} ${mailInfo.student_surname},\nyour thesis application for the ${mailInfo.thesis_title} proposal, supervised by professor ${mailInfo.teacher_surname}, has been ${mailInfo.status ? 'Accepted' : 'Rejected'}.\nBest Regards, Polito Staff.`
+            }),
+        });
+    }
+    return data;
 }
 
 async function updateProposal(proposal) {
-  const id = proposal.id;
-  delete proposal.id;
-    
-  const response = await fetch(`${URL}/teacher/updateProposal/${id}`, {
-    credentials: 'include',
-    method: 'PUT',
-    
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(proposal)
-  });
-  
-  const data = await response.json();
-  return data;
+    const id = proposal.id;
+    delete proposal.id;
+
+    const response = await fetch(`${URL}/teacher/updateProposal/${id}`, {
+        credentials: 'include',
+        method: 'PUT',
+
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(proposal)
+    });
+
+    const data = await response.json();
+    return data;
 }
 
 
-async function deleteProposal(proposalId) {
-  const response = await fetch(`${URL}/teacher/deleteProposal`,{
-    method: 'DELETE',  
-    credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({proposalId:proposalId}),
-  });
-  const data = await response.json();
-  console.log(data);
-  if(response.status==200)
-  {
-    await fetch(`${URL}/send_email`,{
-      method: 'POST',  
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-            recipient_mail: "salvo.cav96@gmail.com",
-            subject: `Info about on your application about ${mailInfo.thesis_title}` ,
-            message: `Hello dear student,\n Unfortunately your thesis application for the ${mailInfo.thesis_title} proposal, supervised by professor ${mailInfo.teacher_surname}, has been cancelled because the thesis proposal was deleted.\nBest Regards, Polito Staff.`
-      }),
+async function deleteProposal(proposalId, mailInfo) {
+    const response = await fetch(`${URL}/teacher/deleteProposal`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ proposalId: proposalId }),
     });
-  }
-  return data;   
+    const data = await response.json();
+    if (response.status == 200) {
+        await fetch(`${URL}/send_email`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                recipient_mail: "salvo.cav96@gmail.com",
+                subject: `Info about on your application about ${mailInfo.thesis_title}`,
+                message: `Hello dear student,\n Unfortunately your thesis application for the ${mailInfo.thesis_title} proposal, supervised by professor ${mailInfo.teacher_surname}, has been cancelled because the thesis proposal was deleted.\nBest Regards, Polito Staff.`
+            }),
+        });
+    }
+    return data;
 }
 
 async function retrieveCoSupervisorsGroups(co_supervisor_array) {
-  const response = await fetch(`${URL}/teacher/retrieveCosupGroup`,{
-    method: 'POST',  
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ cosup_mails : co_supervisor_array }),
-  });
-  const data = await response.json();
-  return data;   
+    const response = await fetch(`${URL}/teacher/retrieveCosupGroup`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cosup_mails: co_supervisor_array }),
+    });
+    const data = await response.json();
+    return data;
 }
 async function archiveProposal(proposalId, status) {
-  
-  const response = await fetch(`${URL}/teacher/ProposalsList/${proposalId}`,{
-    method: 'PATCH',  
-    credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({status:status}),
-  });
-  const data = await response.json();
-  return data;   
+
+    const response = await fetch(`${URL}/teacher/ProposalsList/${proposalId}`, {
+        method: 'PATCH',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ status: status }),
+    });
+    const data = await response.json();
+    return data;
 }
 
 const API = {
-  logInWithSaml,
-  logOutWithSaml,
-  logIn,
-  logOut,
-  getUserInfo,
-  getTeacherProposals,
-  getTeacherDetail,
-  getStudentDetail,
-  getAllProposals,
-  addApplication,
-  getAllTeachers,
-  getAllTypes,
-  getFilteredProposals,
-  getAllKeywords,
-  getAllGroups,
-  getApplicationsList,
-  addProposal,
-  getApplicationsListTeacher,
-  acceptDeclineApplication,
-  updateProposal,
-  setVirtualClock,
-  resetVirtualClock,
-  getAllProposalsForStudent,
-  deleteProposal,
-  retrieveCoSupervisorsGroups,
-  archiveProposal
+    logInWithSaml,
+    logOutWithSaml,
+    logIn,
+    logOut,
+    getUserInfo,
+    getTeacherProposals,
+    getTeacherDetail,
+    getStudentDetail,
+    getAllProposals,
+    addApplication,
+    getAllTeachers,
+    getAllTypes,
+    getFilteredProposals,
+    getAllKeywords,
+    getAllGroups,
+    getApplicationsList,
+    addProposal,
+    getApplicationsListTeacher,
+    acceptDeclineApplication,
+    updateProposal,
+    setVirtualClock,
+    resetVirtualClock,
+    getAllProposalsForStudent,
+    deleteProposal,
+    retrieveCoSupervisorsGroups,
+    archiveProposal
 };
 
 export default API;

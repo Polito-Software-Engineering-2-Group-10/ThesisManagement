72cbb01298f87b2b979d4e2b4415f84845ef11df
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2023-12-12 18:16:38 +0100

    back button no longer needed
    In the new interface, is not necessary to have 2 different  buttons for application and going back to home, as they are the same location.
    
    Fixed from 'search' to '/'

--- frontend/src/pages/BrowseApplicationDecision.jsx
+++ frontend/src/pages/BrowseApplicationDecision.jsx
@@ -41,8 +41,8 @@ function BrowseAppDecision(props){
                     </Table>
                     <Container style={{marginTop: "30px"}}>
                         <Row>
-                            <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}><Button variant="success" onClick={() => navigate('/search')}>New Apply</Button></Col>
-                            <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}><Button variant="danger"  onClick={() => navigate('/')}>Back</Button></Col>
+                            <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}><Button variant="success" onClick={() => navigate('/')}>New Apply</Button></Col>
+                            {/* <Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}><Button variant="danger"  onClick={() => navigate('/')}>Back</Button></Col> */}
                         </Row>
                     </Container>                    
                 </>

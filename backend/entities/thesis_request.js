400e669dc88052a391c297e31b078f5215b626ad
Alessio Rosiello <alex.robassomero@gmail.com>
2024-01-10 14:44:08 +0100

    Fix bug in PATCH API to update thesis request from student side

--- backend/entities/thesis_request.js
+++ backend/entities/thesis_request.js
@@ -89,11 +89,12 @@ class ThesisRequestTable {
     async updateThesisRequest(student_id, request_id, new_request) {
         // set comment to NULL when updating request because this means that allegedly the student has satisfied the teacher's comment
         const PENDING_STATUS = 0; // if this is changed, also change the corresponding value in the frontend
+        const CHANGE_REQUESTED = 2;
         /// ASSUMED STATUSES: 0 = pending, 1 = approved, 2 = request for change, 3 = rejected
-        const query = `UPDATE thesis_request SET title = $3, description = $4, co_supervisor = $6, comment = NULL, status_teacher = ${PENDING_STATUS} WHERE student_id = $1 AND id = $2 RETURNING *`;
+        const query = `UPDATE thesis_request SET title = $3, description = $4, co_supervisor = $5, comment = NULL, status_teacher = ${PENDING_STATUS} WHERE student_id = $1 AND id = $2 AND status_teacher = ${CHANGE_REQUESTED} RETURNING *`;
         const sid = getNum(student_id);
         const rid = getNum(request_id);
-        const result = await this.db.executeQueryExpectOne(query, sid, rid, new_request.title, new_request.description, new_request.co_supervisor, `Failed to update Request`);
+        const result = await this.db.executeQueryExpectOne(query, sid, rid, new_request.title, new_request.description, new_request.co_supervisor, `No request found to be updated`);
         return ThesisRequest.fromRow(result);
     }
 

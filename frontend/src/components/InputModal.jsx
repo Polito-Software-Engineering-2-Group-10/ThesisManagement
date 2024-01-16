0b756854274c6b61ff4b6088d564c2ed8872fae9
SalvatoreCavallaro1 <135614562+SalvatoreCavallaro1@users.noreply.github.com>
2024-01-16 12:11:53 +0100

    fix comment bug

--- frontend/src/components/InputModal.jsx
+++ frontend/src/components/InputModal.jsx
@@ -1,5 +1,5 @@
 import {Modal, Button} from 'react-bootstrap';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 
 import {Form} from 'react-bootstrap';
 
@@ -7,33 +7,49 @@ import "../index.css"
 
 function ConfirmModal(props){
     const { show, setShow } = props;
-    const [value, setValue]=useState("");
+    const [value, setValue]=useState('');
+
+    useEffect(() => {
+        props.setComment(value);
+        console.log(value);
+        
+      }, [value]);
 
     return (
-        <Modal show={show} onHide={() => setShow(false)}>
-            <Modal.Header closeButton>
-                <Modal.Title>{props.title}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Comments</Form.Label>
-                    <Form.Control as="textarea" value={value} onChange={(event)=> {
-                        setValue(event.target.value);
-                    }} onInput={()=>{props.setComment(value);}}/>
-                </Form.Group>
-            </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="confirm" onClick={() => {setShow(false); console.log("value vale => "+value); props.onConfirm();}}>
-                   Suggest changes 
-                </Button>
-                <Button variant="cancel" onClick={() => setShow(false)}>
-                    Cancel
-                </Button>
-                
-            </Modal.Footer>
-        </Modal>
+      <Modal show={show} onHide={() => {setShow(false); setValue("");}}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Comments</Form.Label>
+              <Form.Control
+                as="textarea"
+                type="text"
+                name="comment"
+                value={value}
+                onChange={(ev) => setValue(ev.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="confirm"
+            onClick={() => {
+              setShow(false);
+              console.log("value vale => " + value);
+              props.onConfirm();
+            }}
+          >
+            Suggest changes
+          </Button>
+          <Button variant="cancel" onClick={() => {setShow(false); setValue("");}}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
 }
 
